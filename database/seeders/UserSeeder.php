d4c30380d031af4145f9f2a2f62f778f3b636a89
Sena Kendali <sena.kendali@gmail.com>
2026-04-14 14:35:59 +0700

    Add sedder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,17 +11,61 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Admin Ops',
-            'email' => 'admin@flexlabs.co.id',
+            'name' => 'Sena Kendali',
+            'email' => 'sena@flexlabs.co.id',
             'role' => 'admin',
             'password' => Hash::make('password123'),
         ]);
 
         User::create([
-            'name' => 'Instruktur 1',
-            'email' => 'instruktur1@flexlabs.co.id',
-            'role' => 'instructor',
+            'name' => 'Awalokita',
+            'email' => 'awalokita@flexlabs.co.id',
+            'role' => 'admin',
             'password' => Hash::make('password123'),
         ]);
+
+        User::create([
+            'name' => 'Hanif',
+            'email' => 'kasetyan@flexlabs.co.id',
+            'role' => 'marketing',
+            'password' => Hash::make('password123'),
+        ]);
+
+        User::create([
+            'name' => 'Irfan',
+            'email' => 'irfan@flexlabs.co.id',
+            'role' => 'marketing',
+            'password' => Hash::make('password123'),
+        ]);
+
+         User::create([
+            'name' => 'Syahreza',
+            'email' => 'syahreza@flexlabs.co.id',
+            'role' => 'marketing',
+            'password' => Hash::make('password123'),
+        ]);
+
+        User::create([
+            'name' => 'Fajar',
+            'email' => 'fajar@flexlabs.co.id',
+            'role' => 'academic',
+            'password' => Hash::make('password123'),
+        ]);
+
+         User::create([
+            'name' => 'Faldi',
+            'email' => 'hr@flexlabs.co.id',
+            'role' => 'marketing',
+            'password' => Hash::make('password123'),
+        ]);
+
+         User::create([
+            'name' => 'Gita',
+            'email' => 'finance@flexlabs.co.id',
+            'role' => 'marketing',
+            'password' => Hash::make('password123'),
+        ]);
+
+       
     }
 }
