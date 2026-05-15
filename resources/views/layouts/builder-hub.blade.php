775d71e3f20d6bc40265a2a4cac63035d75d3e0d
Sena Kendali <sena.kendali@gmail.com>
2026-05-16 06:41:11 +0700

    Update sidebar behaviour

--- resources/views/layouts/builder-hub.blade.php
+++ resources/views/layouts/builder-hub.blade.php
@@ -397,8 +397,8 @@
                 @endif
 
                 <div class="builder-body-grid grid flex-1 grid-cols-1 items-start gap-5 lg:grid-cols-[360px_minmax(0,1fr)] xl:grid-cols-[390px_minmax(0,1fr)]">
-                    <aside class="builder-sidebar hidden self-start lg:block">
-                        <div class="sticky top-[118px] rounded-[2rem] border border-white/80 bg-white p-6 shadow-soft">
+                    <aside class="builder-sidebar sticky top-[118px] hidden self-start lg:block">
+                        <div class="rounded-[2rem] border border-white/80 bg-white p-6 shadow-soft">
                             @hasSection('sidebar')
                                 @yield('sidebar')
                             @else
