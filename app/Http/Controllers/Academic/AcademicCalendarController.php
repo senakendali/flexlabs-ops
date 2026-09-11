6a5339dbbfd26e6b3e365bdf6daac9349bccf77a
Sena Kendali <sena.kendali@gmail.com>
2026-09-11 17:02:07 +0700

    Update filter batch

--- app/Http/Controllers/Academic/AcademicCalendarController.php
+++ app/Http/Controllers/Academic/AcademicCalendarController.php
@@ -52,7 +52,7 @@ class AcademicCalendarController extends Controller
             ->where(function ($query) {
                 $query->whereNull('status')
                     ->orWhereRaw(
-                        "LOWER(TRIM(status)) NOT IN (?, ?, ?, ?)",
+                        "LOWER(TRIM(status)) NOT IN (?, ?, ?, ?, ?)",
                         [
                             'open',
                             'completed',
