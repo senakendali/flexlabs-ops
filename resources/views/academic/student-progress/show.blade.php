7fe51c84c069ee76c6e5702c863dad7b2c9ec1a6
Sena Kendali <sena.kendali@gmail.com>
2026-09-18 16:43:20 +0700

    Update progress style

--- resources/views/academic/student-progress/show.blade.php
+++ resources/views/academic/student-progress/show.blade.php
@@ -725,7 +725,7 @@
                                             style="height: 9px; border-radius: 999px;"
                                         >
                                             <div
-                                                class="progress-bar bg-primary"
+                                                class="progress-bar"
                                                 role="progressbar"
                                                 style="width: {{ $progressValue }}%; border-radius: 999px;"
                                                 aria-valuenow="{{ $progressValue }}"
